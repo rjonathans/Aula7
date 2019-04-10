34412876fdf1c396d12807dde489ff5d139cdd0c
Digital <i0kiokm>
2019-04-10 19:30:46 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/Aviao.java
@@ -0,0 +1,35 @@
+package br.com.digitalhouse;
+
+public class Aviao implements Voador{
+    private String tipoDeAsa;
+    private int qtdAssentos;
+
+    public Aviao(){
+    }
+
+    public Aviao(int quantidadeDeAssentos,String tipoAsa){
+        qtdAssentos=quantidadeDeAssentos;
+        tipoDeAsa=tipoAsa;
+    }
+
+    public String getTipoDeAsa() {
+        return tipoDeAsa;
+    }
+
+    public void setTipoDeAsa(String tipoAsa) {
+        tipoDeAsa = tipoAsa;
+    }
+
+    public Integer getQtdAssentos() {
+        return qtdAssentos;
+    }
+
+    public void setQtdAssentos(int qtd) {
+        qtdAssentos = qtd;
+    }
+
+    @Override
+    public void voar() {
+        System.out.println("Voando como um avião....");
+    }
+}
